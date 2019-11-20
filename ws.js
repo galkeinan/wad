912e897a90ed6871340dc64bdbc100eafd5fd86e
ubuntu <galkeinan22@gmail.com>
2019-11-20 16:22:21 +0200

    filter only presence frames

--- ws.js
+++ ws.js
@@ -1,28 +1,27 @@
-const wsa = new window.WebSocket('ws://localhost:9000', 'wa-event');
+var wsa = new window.WebSocket('ws://localhost:9000', 'wa-event');
 wsa.onmessage = function(msg) {
   console.log('[wsa#msg]', msg.data);
 };
 wsa.onerror = function(msg) {
-  console.error('wsa#err');
+  console.error('[wsa#err]');
   console.debug(msg);
 };
-var Main = function() {
-  Notification.requestPermission();
-  WebSocket.prototype._send = WebSocket.prototype.send;
 
+(function() {
+  WebSocket.prototype._send = WebSocket.prototype.send;
   WebSocket.prototype.send = function(data) {
     this._send(data);
-    console.log('[WebSocket#send]', data);
 
     this.addEventListener('message', function(msg) {
-      console.log('[WebSocket#msg]', data);
-      wsa.send(data);
+      console.log('>> ' + msg.data);
+      if (msg.data && msg.data.includes('Presence')) {
+        wsa.send(msg.data);    
+      }
     }, false);
 
     this.send = function(data) {
-      this._send(data);         
+      this._send(data);
+      console.log("<< " + data);
     };
   }
-};
-
-Main();
+})();
